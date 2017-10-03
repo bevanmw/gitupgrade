5909f69063852f37ae5e980513904aef9fc95701
Bevan Wentzel <bevan@teamgeek.co.za>
2017-10-03 11:14:21 +0200

    Updated version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='upgradegit',
-    version='0.0.6',
+    version='0.0.7',
     license='BSD',
     description='Upgrade PIP and package.json git to latest HEAD',
     url='https://bitbucket.org/mybucks/upgrade',
